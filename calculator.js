9ef45aab09a9fdcb90ea04828348c8c5a297da52
Leonardo Soares <leonaardo.soaares@gmail.com>
2020-07-23 14:36:01 -0300

    Refatorando código e Estilização

--- calculator.js
+++ calculator.js
@@ -1,46 +1,90 @@
-var n1 = null;
-var n2 = null;
-var operation = null;
-
-$(document).ready(function(){
-    $(".operation").on('click' , function(){
-        operation = this.value;
-        console.log(operation)
-    })
-    $(".numbers").on('click' , function(){
-        $("#visor-input").val(this.value);
-        if (n1 == null) {
-              n1 = this.value;       
-        } else {
-            n2 = this.value;
+function getHistory() {
+    return document.getElementById("history-value").innerText;
+  }
+  function printHistory(num) {
+    document.getElementById("history-value").innerText = num;
+  }
+  function getOutput() {
+    return document.getElementById("output-value").innerText;
+  }
+  function printOutput(num) {
+    if (num == "") {
+      document.getElementById("output-value").innerText = num;
+    } else {
+      document.getElementById("output-value").innerText = getFormattedNumber(num);
+    }
+  }
+  function getFormattedNumber(num) {
+    if (num == "-") {
+      return "";
+    }
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+  }
+  function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g, ""));
+  }
+  
+  var operator = document.getElementsByClassName("operator");
+  for (var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener("click", function () {
+      if (this.id == "clear") {
+        printHistory("");
+        printOutput("");
+      } else if (this.id == "backspace") {
+        var output = reverseNumberFormat(getOutput()).toString();
+        if (output) {
+          output = output.substr(0, output.length - 1);
+          printOutput(output);
         }
-        console.log(n1, n2);
-    })
-    $(".result").on('click' , function(){
-        result(parseInt(n1), parseInt(n2), operation);
-    })
-})
-
-function result(n1, n2, operation){
-    var result = 0;
-    switch (operation) {
-        case '+':
-            result = n1 + n2;
-            break;
-
-        case '-':
-            result = n1 - n2;
-            break;
-
-        case '*':
-            result = n1 * n2;
-            break;
-        
-        case '/':
-            result = n1 / n2;
-            break;        
-        default:
-            break;
-    }   
-    console.log(result)
-}
+      } else {
+        var output = getOutput();
+        var history = getHistory();
+        if (output == "" && history != "") {
+          if (isNaN(history[history.length - 1])) {
+            history = history.substr(0, history.length - 1);
+          }
+        }
+        if (output != "" || history != "") {
+          output = output == "" ? output : reverseNumberFormat(output);
+          history = history + output;
+          if (this.id == "=") {
+            var result = eval(history);
+            printOutput(result);
+            printHistory("");
+          } else if (this.id == "%") {
+            var n = reverseNumberFormat(getOutput());
+            var percent = n / 100;
+            printOutput(percent.toFixed(4));
+          } else {
+            history = history + this.id;
+            printHistory(history);
+            printOutput("");
+          }
+        }
+      }
+    });
+  }
+  
+  var number = document.getElementsByClassName("number");
+  for (var i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", function () {
+      var output = reverseNumberFormat(getOutput());
+      //if output is a number
+      if (output != NaN) {
+        output = output + this.id;
+        printOutput(output);
+      }
+    });
+  }
+  
+  let checkbox = document.querySelector("input[name=theme]");
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  });
+  
